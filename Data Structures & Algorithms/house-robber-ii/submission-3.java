1a37427efc7b92cd17e00fb95b0df886a8bbd363
Anvarjon <142890754+Anvarjon7@users.noreply.github.com>
2026-07-27 16:59:37 +0200

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0], nums[1]);
+
+        return Math.max(countMax(nums, 1, n), countMax(nums, 0, n - 1));
+    }
+
+
+    int countMax(int nums[], int start, int end) {
+        int prev2 = 0, prev1 = 0;
+
+        for(int i = start; i < end; i++) {
+            int curr = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
